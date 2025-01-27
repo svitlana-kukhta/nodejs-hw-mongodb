74a06d2f959dbdea748e5810836648718b46dfa0
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-01-27 17:28:19 +0200

    goit-nodejs-hw-03-at03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
 import { initMongoDB } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
+
+dotenv.config();
+console.log('PORT:', process.env.PORT);
+console.log('MONGODB_USER:', process.env.MONGODB_USER);
+console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD ? 'Loaded' : 'Not loaded');
+console.log('MONGODB_URL:', process.env.MONGODB_URL);
+console.log('MONGODB_DB:', process.env.MONGODB_DB);
 
 const bootstrap = async () => {
   await initMongoDB();
