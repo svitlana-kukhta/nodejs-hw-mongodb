1ff3ab45ba80ac6b9295cfd893ca5d2ff9c7fb1d
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-02-03 15:13:48 +0200

    goit-nodejs-hw-04-04

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,5 +1,5 @@
 export const calculatePaginationData = (count, perPage, page) => {
-  const totalPages = Math.ceil(count / perPage);
+  const totalPages = count > 0 ? Math.ceil(count / perPage) : 0;
   const hasNextPage = page < totalPages;
   const hasPreviousPage = page > 1;
 
