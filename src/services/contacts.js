46578c1668942964d646dc91eac5915f1eddfc60
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-02-03 15:13:48 +0200

    goit-nodejs-hw-04-04

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -16,15 +16,17 @@ if (filter.contactType) {
   }
 if (filter.isFavourite !== undefined) {
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
-  }
-const [contactsCount, contacts] = await Promise.all([
-    ContactsCollection.find().merge(contactsQuery).countDocuments(),
-    contactsQuery
-      .skip(skip)
-      .limit(limit)
-      .sort({ [sortBy]: sortOrder })
-      .exec(),
-  ]);
+}
+
+const contactsCount = await ContactsCollection.find()
+  .merge(contactsQuery)
+  .countDocuments();
+
+const contacts = await contactsQuery
+  .skip(skip)
+  .limit(limit)
+  .sort({ [sortBy]: sortOrder })
+  .exec();
 
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
