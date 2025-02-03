0a75d7aba6f1b34521f2359e9b72d05e186d637b
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-02-03 15:56:44 +0200

    goit-nodejs-hw-04-05

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -18,15 +18,14 @@ if (filter.isFavourite !== undefined) {
     contactsQuery.where('isFavourite').equals(filter.isFavourite);
 }
 
-const contactsCount = await ContactsCollection.find()
-  .merge(contactsQuery)
-  .countDocuments();
-
-const contacts = await contactsQuery
-  .skip(skip)
-  .limit(limit)
-  .sort({ [sortBy]: sortOrder })
-  .exec();
+const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
 
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
