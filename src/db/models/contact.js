0017352363cf1178a5c3294f47f3d7c4d31b2ca3
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-02-10 19:18:39 +0200

    goit-nodejs-hw-05-01

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -38,19 +38,3 @@ const contactsSchema = new Schema(
 
 export const ContactsCollection = model('contacts', contactsSchema);
 
-const usersSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true, versionKey: false },
-);
-
-usersSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.password;
-  return obj;
-};
-
-export const UsersCollection = model('users', usersSchema);
