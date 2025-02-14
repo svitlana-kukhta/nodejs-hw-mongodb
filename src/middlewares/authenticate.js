db60a7497c827a021fdbb59729d314295ee945b6
svitlana-kukhta <tyschenkosvetlana@gmail.com>
2025-02-14 14:21:00 +0200

    userId

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -35,10 +35,6 @@ export const authenticate = async (req, res, next) => {
     next(createHttpError(401, 'Access token expired'));
   }
 
-  console.log('Session:', session);
-  console.log('Session userId type:', typeof session.userId);
-  console.log('Session userId value:', session.userId);
-
   const user = await UsersCollection.findById(session.userId);
 
   if (!user) {
